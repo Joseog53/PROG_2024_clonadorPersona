a01f332d72dde1fa3cda6f8d983172fbe1d51696
Ortega <Ortega@DESKTOP-7D19EM5>
2024-04-26 09:34:23 +0200

    VALIDADOR DE PERSONAS

--- clonadorPersona/src/persona/ClonadorPersona.java
+++ clonadorPersona/src/persona/ClonadorPersona.java
@@ -5,15 +5,17 @@ import java.util.List;
 import java.util.Random;
 
 import utilidades.FactoryPersona;
+import utilidades.ValidadorPersona;
 
 public class ClonadorPersona {
 
 	private String nombre;
 
 	List<Persona> personas;
-	
+
 	/**
 	 * Constructor de ClonadorPersona
+	 * 
 	 * @param nombre
 	 */
 
@@ -21,9 +23,10 @@ public class ClonadorPersona {
 
 		this.nombre = nombre;
 	}
-	
+
 	/**
 	 * Operación que genera 10 clones aleatoriamente
+	 * 
 	 * @return List
 	 */
 
@@ -31,13 +34,18 @@ public class ClonadorPersona {
 
 		List<Persona> personas = new ArrayList<Persona>();
 
-		
-
 		for (int i = 0; i < 10; i++) {
 
-			Persona p = FactoryPersona.getPersona( nombre);
-			
-			personas.add(0,p);
+			Persona p = FactoryPersona.getPersona(nombre);
+
+			boolean personaValida = ValidadorPersona.isValid(p);
+
+			if (personaValida) {
+
+				personas.add(0, p);
+
+			}
+
 		}
 
 		return personas;
---
+++ clonadorPersona/src/persona/PersonaData.java
@@ -0,0 +1,13 @@
+package persona;
+
+public class PersonaData {
+
+	public static final int PESO_MIN = 20;
+	public static final int PESO_MAX = 70;
+	public static final int EDAD_MIN = 18;
+	public static final int EDAD_MAX = 70;
+	public static final int ALTURA_MIN = 120;
+	public static final int ALTURA_MAX = 200;
+
+	
+}
